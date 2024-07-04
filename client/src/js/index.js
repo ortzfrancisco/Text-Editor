e329c378daaa444e2af6f97a6ee100a8b5e34f60
francisco ortiz <franciscoortz89@gmail.com>
2024-07-04 17:38:23 -0400

    update index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,32 +2,51 @@ import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
+import { putDb } from './database';
+import { put } from '../../../server/routes/htmlRoutes';
 
-const main = document.querySelector('#main');
-main.innerHTML = '';
+window.addEventListener('load', async () => {
+    const editor = document.getElementById('editor');
 
-const loadSpinner = () => {
-    const spinner = document.createElement('div');
-    spinner.classList.add('spinner');
-    spinner.innerHTML = `
+    // Get content from the database
+    const content = await getDb();
+    if (content) {
+        editor.value = content;
+    }
+
+    // Save content to the database
+    document.getElementById('saveButton').addEventListener('click', async () => {
+        const content = editor.value;
+        putDb(content);
+    }
+    );
+});
+
+    const main = document.querySelector('#main');
+    main.innerHTML = '';
+
+    const loadSpinner = () => {
+        const spinner = document.createElement('div');
+        spinner.classList.add('spinner');
+        spinner.innerHTML = `
   <div class="loading-container">
   <div class="loading-spinner" />
   </div>
   `;
-    main.appendChild(spinner);
-};
+        main.appendChild(spinner);
+    };
 
-const editor = new Editor();
+    const editor = new Editor();
 
-if (typeof editor === 'undefined') {
-    loadSpinner();
-}
+    if (typeof editor === 'undefined') {
+        loadSpinner();
+    }
 
-// Check if service workers are supported
-if ('serviceWorker' in navigator) {
-    // register workbox service worker
-    const workboxSW = new Workbox('/src-sw.js');
-    workboxSW.register();
-} else {
-    console.error('Service workers are not supported in this browser.');
-}
+    // Check if service workers are supported
+    if ('serviceWorker' in navigator) {
+        // register workbox service worker
+        const workboxSW = new Workbox('/src-sw.js');
+        workboxSW.register();
+    } else {
+        console.error('Service workers are not supported in this browser.');
+    }
